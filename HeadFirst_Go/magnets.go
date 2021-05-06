461a6bfa7dc174c7ae5d0e6577fbc96e77f5f9bf
ABDNdev <igor@abidin.ru>
2021-05-06 18:04:05 +0300

    Modify file

--- HeadFirst_Go/magnets.go
+++ HeadFirst_Go/magnets.go
@@ -6,9 +6,8 @@ import (
 
 func main() {
 	var originalCount int = 10
-	var eatenCount int = 4
-
 	fmt.Println("I started with", originalCount, "apples.")
+	var eatenCount int = 4
 	fmt.Println("Some jerk ate", eatenCount, "apples.")
 	fmt.Println("There are", originalCount-eatenCount, "apples left.")
 }
